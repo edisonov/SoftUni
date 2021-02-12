78e4531552677e4da8977dd780aff7f09ffe2edc
Emil Edisonov <76521869+higemon82@users.noreply.github.com>
2021-02-12 16:50:48 +0200

    add file

---
+++ C# Advanced/Generics - Exercise/CustomTuple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomTuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] splittedFirst = Console.ReadLine().Split();
+            string fullName = $"{splittedFirst[0]} {splittedFirst[1]}";
+            List<string> townData = splittedFirst.Skip(3).ToList();
+            string town = string.Join(" ", townData);
+
+            Threeuple<string, string, string> firstThreeuple =
+                new Threeuple<string, string, string>(fullName, splittedFirst[2], town);
+
+            string[] splittedSecond = Console.ReadLine().Split();
+            bool drunk = splittedSecond[2] == "drunk";
+
+            Threeuple<string, int, bool> secondThreeuple =
+                new Threeuple<string, int, bool>
+                (splittedSecond[0], int.Parse(splittedSecond[1]), drunk);
+
+            string[] splittedThird = Console.ReadLine().Split();
+
+            Threeuple<string, double, string> thirdThreeuple =
+                new Threeuple<string, double, string>
+                (splittedThird[0], double.Parse(splittedThird[1]), splittedThird[2]);
+
+            Console.WriteLine(firstThreeuple);
+            Console.WriteLine(secondThreeuple);
+            Console.WriteLine(thirdThreeuple);
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/GenericBoxOfString/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //List<Box<string>> boxes = new List<Box<string>>();
+            List<Box<double>> boxes = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                //Box<string> box = new Box<string>(Console.ReadLine());
+                Box<double> box = new Box<double>(double.Parse(Console.ReadLine()));
+                boxes.Add(box);
+            }
+
+            double value = double.Parse(Console.ReadLine());
+
+            Box<double> comparableBox = new Box<double>(value);
+
+            int count = GetGreaterThanCount(boxes, comparableBox);
+            Console.WriteLine(count);
+            
+        }
+
+        private static int GetGreaterThanCount<T>(List<Box<T>> boxes, Box<T> box)
+            where T: IComparable
+        {
+            int count = 0;
+
+            foreach (Box<T> item in boxes)
+            {
+                int compare = item.Value.CompareTo(box.Value);
+                if (compare > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static void SwapIndexes<T>(List<Box<T>> boxes, int firstIndex, int secondIndex) 
+        {
+            Box<T> temp = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
