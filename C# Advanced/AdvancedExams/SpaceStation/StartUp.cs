8d3afd9ed410a24491e52764fee18050dc28ea1b
Emil Edisonov <76521869+higemon82@users.noreply.github.com>
2021-02-17 18:15:15 +0200

    add file

---
+++ C# Advanced/AdvancedExams/SpaceStation/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpaceStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
