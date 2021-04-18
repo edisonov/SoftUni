ab14366a1219cb040ec8d07347a2f974d3de74a8
Emil Edisonov <76521869+edisonov@users.noreply.github.com>
2021-04-18 07:47:25 +0300

    Create Product.cs

---
+++ C# OOP/EXAM/test/Models/Products/Product.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnlineShop.Common.Constants;
+
+namespace OnlineShop.Models.Products
+{
+    public abstract class Product : IProduct
+    {
+        private int id;
+        private string manufacturer;
+        private string model;
+        private decimal price;
+        protected double overallPerformance;
+
+        protected Product(int id, string manufacturer, string model, decimal price, double overallPerformance)
+        {
+            this.Id = id;
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Price = price;
+            this.OverallPerformance = overallPerformance;
+        }
+
+        public int Id
+        {
+            get => id;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidProductId);
+                }
+
+                this.id = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get => this.manufacturer;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidManufacturer);
+                }
+
+                this.manufacturer = value;
+            }
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidModel);
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get => this.price;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+            }
+        }
+
+        public double OverallPerformance
+        {
+            get => this.overallPerformance;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOverallPerformance);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Overall Performance: {this.OverallPerformance}. " +
+                   $"Price: {this.Price} - {this.GetType().Name}: {this.Manufacturer} {this.Model} (Id: {this.Id})";
+        }
+    }
+}
