a1a1ff47e3047ba88917e18b9ca032ba072584ee
Emil Edisonov <76521869+edisonov@users.noreply.github.com>
2021-04-18 07:46:05 +0300

    Create PowerSupply.cs

---
+++ C# OOP/EXAM/test/Models/Products/Components/PowerSupply.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShop.Models.Products.Components
+{
+    public class PowerSupply : Component
+    {
+        private double powerSupply = 1.05;
+
+        public PowerSupply(int id, string manufacturer, string model,
+            decimal price, double overallPerformance, int generation)
+            : base(id, manufacturer, model, price, overallPerformance, generation)
+        {
+            this.overallPerformance *= powerSupply;
+        }
+    }
+}
